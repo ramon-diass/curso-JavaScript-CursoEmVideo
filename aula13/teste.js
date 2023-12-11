f427b5e556296838af7e1feb27148e07d3be546f
Ramon Dias <123565423+ramon-diass@users.noreply.github.com>
2023-12-11 12:54:25 -0300

    while em JS

---
+++ aula13/teste.js
@@ -0,0 +1,18 @@
+/*
+console.log('Olá, tudo bem?')
+console.log('Olá, tudo bem?')
+console.log('Olá, tudo bem?')
+console.log('Olá, tudo bem?')
+console.log('Olá, tudo bem?')
+*/
+
+var cont = 1
+while (cont <= 6) {
+    console.log(`Passo ${cont}`)
+    cont++
+}
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+} while (c < 1)
